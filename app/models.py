fa36045d219b8840d9bae22070ec8469563cc9f4
Krys Fernando <dev@krysfernando.com>
2025-06-10 04:09:49 -0400

    add category endpoints

--- app/models.py
+++ app/models.py
@@ -6,3 +6,8 @@ class User(Base):
     __tablename__ = "users"
     id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
     username: Mapped[str] = mapped_column(String(50), nullable=False, unique=True, index=True)
+
+class Category(Base):
+    __tablename__ = "categories"
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
+    category_name: Mapped[str] = mapped_column(String(50), unique=True)
